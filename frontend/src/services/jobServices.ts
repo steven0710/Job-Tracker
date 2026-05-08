81f7d148a942602a4efd146f1bd5336642626136
steven0710 <stevenhujuly10@gmail.com>
2026-05-07 19:09:24 -0500

    created staging branch(first)

--- frontend/src/services/jobServices.ts
+++ frontend/src/services/jobServices.ts
@@ -1,20 +1,6 @@
 import { BACKEND_API } from "../constants";
 import type { Job } from "../types/jobs";
 
-// const STORAGE_KEY = "jobs";
-
-// export async function getJobs(): Promise<Job[]> {
-//   const saved = localStorage.getItem(STORAGE_KEY);
-//   return saved ? JSON.parse(saved) : [];
-// }
-
-// export async function addJob(job: Job): Promise<Job[]> {
-//   const jobs = await getJobs();
-//   const updated = [...jobs, job];
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
-//   return updated;
-// }
-
 export async function getJobsByApi(): Promise<{ jobs: Job[] }> {
   const token = localStorage.getItem("token");
   const res = await fetch(`${BACKEND_API}/jobs`, {
